28da37d699724d318add42eea73ac016ceac0647
Fares Emanuel Gómez Cortés <fe.gomez.corter@gmail.com>
2022-07-16 11:40:05 -0500

    feat: Add log branches
    Signed-off-by: Fares Emanuel Gómez Cortés <fe.gomez.corter@gmail.com>

--- Timber.cpp
+++ Timber.cpp
@@ -4,6 +4,21 @@
 // Make code easier to type with "using namespace"
 using namespace sf;
 
+// Function declaration
+void updateBranches(int seed);
+
+const int NUM_BRANCHES = 6;
+Sprite branches[NUM_BRANCHES];
+
+// Where is the player/branch?
+// Left or Right
+enum class side {
+    LEFT, RIGHT, NONE
+};
+side branchPositions[NUM_BRANCHES];
+
+const int NUM_CLOUDS = 3;
+
 // This is where our game starts from
 int main() {
     // Create a video mode object
@@ -54,9 +69,9 @@ int main() {
     textureCloud.loadFromFile("../graphics/cloud.png");
 
     // 3 New sprites with the same texture
-    Sprite cloudSprites[3];
-    bool cloudsActive[3];
-    float cloudsSpeed[3];
+    Sprite cloudSprites[NUM_CLOUDS];
+    bool cloudsActive[NUM_CLOUDS];
+    float cloudsSpeed[NUM_CLOUDS];
     int cloudsAmount = sizeof(cloudSprites) / sizeof(Sprite);
 
     for (int i = 0; i < cloudsAmount; i++) {
@@ -125,6 +140,20 @@ int main() {
     messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
 
     scoreText.setPosition(20, 20);
+
+    // Prepare 6 branches
+    Texture textureBranch;
+    textureBranch.loadFromFile("../graphics/branch.png");
+
+    // Set the texture for each branch sprite
+    for (int i = 0; i < NUM_BRANCHES; ++i) {
+        branches[i].setTexture(textureBranch);
+        branches[i].setPosition(-2000, 2000);
+
+        // Set the sprite's origin to dead centre
+        // We can then spin it round without changing its position
+        branches[i].setOrigin(220, 20);
+    }
 
     while (window.isOpen()) {
         /*
@@ -230,6 +259,28 @@ int main() {
             std::stringstream ss;
             ss << "Score = " << score;
             scoreText.setString(ss.str());
+
+            // Update the branch sprites
+            for (int i = 0; i < NUM_BRANCHES; ++i) {
+                float height = i * 150;
+
+                if (branchPositions[i] == side::LEFT) {
+                    // Move the sprite to the left side
+                    branches[i].setPosition(610, height);
+
+                    // Flip the sprite round the other way
+                    branches[i].setRotation(180);
+                } else if (branchPositions[i] == side::RIGHT) {
+                    // Move the sprite to the right side
+                    branches[i].setPosition(1330, height);
+
+                    // Set the sprite rotation to normal
+                    branches[i].setRotation(0);
+                } else {
+                    // Hide the branch
+                    branches[i].setPosition(3000, height);
+                }
+            }
         } // End if (!paused)
 
         /*
@@ -247,6 +298,11 @@ int main() {
         // Draw the clouds
         for (int i = 0; i < cloudsAmount; ++i) {
             window.draw(cloudSprites[i]);
+        }
+
+        // Draw the branches
+        for (int i = 0; i < NUM_BRANCHES; ++i) {
+            window.draw(branches[i]);
         }
 
         // Draw the tree
@@ -271,4 +327,28 @@ int main() {
     }
 
     return 0;
+}
+
+void updateBranches(int seed) {
+    // Move all the branches down one place
+    for (int j = NUM_BRANCHES - 1; j > 0; --j) {
+        branchPositions[j] = branchPositions[j - 1];
+    }
+
+    // Spawn a new branch at position 0
+    // LEFT, RIGHT or NONE
+    srand((int) time(0) + seed);
+    int r = (rand() % 5);
+
+    switch (r) {
+        case 0:
+            branchPositions[0] = side::LEFT;
+            break;
+        case 1:
+            branchPositions[0] = side::RIGHT;
+            break;
+        default:
+            branchPositions[0] = side::NONE;
+            break;
+    }
 }
